49cb1619b42dd758f40d35bcc072f3fad2473bea
iTeletubi <126301452+RenzoHGR@users.noreply.github.com>
2024-05-10 22:24:27 -0500

    decrypt a Caesar cipher.py

---
+++ decrypt a Caesar cipher.py
@@ -0,0 +1,20 @@
+def descifrar_cifrado_cesar(cadena_cifrada, desplazamiento):
+    cadena_descifrada = ""
+    for caracter in cadena_cifrada:
+        if caracter.isalpha():
+            if caracter.islower():
+                nueva_posicion = (ord(caracter) - ord('a') - desplazamiento) % 26 + ord('a')
+                caracter_descifrado = chr(nueva_posicion)
+            elif caracter.isupper():
+                nueva_posicion = (ord(caracter) - ord('A') - desplazamiento) % 26 + ord('A')
+                caracter_descifrado = chr(nueva_posicion)
+        else:
+            caracter_descifrado = caracter
+        cadena_descifrada += caracter_descifrado
+    return cadena_descifrada
+
+cadena_cifrada = input("Inserta la palabra cifrada: ")
+desplazamiento = int(input("Inserta el valor de desplazamiento: "))
+
+resultado = descifrar_cifrado_cesar(cadena_cifrada, desplazamiento)
+print("La palabra descifrada es:", resultado)
